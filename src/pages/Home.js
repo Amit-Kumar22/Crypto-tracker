81cb08545fb132053744f2ac75f29c7980ef606a
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2024-09-03 16:08:26 +0530

    cryptomy

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainComponent from '../Components/LandingPage/MainComponent'
+import Header from '../Components/Common/Header'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <MainComponent/>  
+    </div>
+  )
+}
+
+export default Home
